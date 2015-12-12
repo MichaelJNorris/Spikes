ef161d2b97ef616a8bd26022dab790d4c9eede07
Michael J Norris <mikejnorris@gmail.com>
2015-12-12 11:04:37 +1100

    first commit

---
+++ Spikes/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Spikes
+//
+//  Created by Michael Norris on 12/12/2015.
+//  Copyright (c) 2015 GratingSoundArt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
